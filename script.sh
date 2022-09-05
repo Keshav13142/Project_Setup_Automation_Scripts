7063c37a85c19d67729a8b34c3eeb1b7e1e53bee
Keshav S <s.keshav13142@gmail.com>
2022-09-05 10:55:55 +0530

    Added setup for express project and also initializes git in every project

--- script.sh
+++ script.sh
@@ -14,10 +14,17 @@ case $TYPE in
             npm i --prefix $APP_NAME;
             ;;
         "react-node")
+            git init
             mkdir backend;
             cp E:/scripts/react-node/package.json .;
             npm create vite@latest frontend -- --template react 
-            cp E:/scripts/index.js ./backend && cp E:/scripts/\.env . && cp E:/scripts/nodemon.json .;
+            cp E:/scripts/index.js ./backend && cp E:/scripts/\.env . && cp E:/scripts/nodemon.json . && cp E:/scripts/.gitignore .;
+            npm i express dotenv
+            ;;
+        "express")
+            git init
+            cp E:/scripts/react-node/package.json .;
+            cp E:/scripts/index.js . && cp E:/scripts/\.env . && cp E:/scripts/nodemon.json . && cp E:/scripts/.gitignore .;
             npm i express dotenv
             ;;
         "solid")
@@ -25,15 +32,17 @@ case $TYPE in
             npm i --prefix $APP_NAME;
             ;;
         "solid-node")
+            git init
             mkdir backend;
             cp E:/scripts/solid-node/package.json .;
             npx degit solidjs/templates/js frontend;
             npm i --prefix frontend;
-            cp E:/scripts/index.js ./backend && cp E:/scripts/\.env . && cp E:/scripts/nodemon.json .;
+            cp E:/scripts/index.js ./backend && cp E:/scripts/\.env . && cp E:/scripts/nodemon.json . && cp E:/scripts/.gitignore .;
             npm i express dotenv
             ;;
         "vanilla")
-            cp E:/scripts/vanilla/* .;
+            git init
+            cp E:/scripts/vanilla/* . && cp E:/scripts/.gitignore .;
             ;;
         *) echo "invalid option $TYPE";;
 esac
